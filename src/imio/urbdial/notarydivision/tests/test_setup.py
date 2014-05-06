2db675a9eb77be1d331fea18c541128db08aceb5
Simon Delcourt <delcourt.simon@gmail.com>
2014-05-06 14:37:38 +0200

    #8008 add tests

--- src/imio/urbdial/notarydivision/tests/test_setup.py
+++ src/imio/urbdial/notarydivision/tests/test_setup.py
@@ -13,42 +13,53 @@ class TestInstallDependencies(unittest.TestCase):
     layer = NAKED_PLONE_INTEGRATION
 
     def setUp(self):
-        """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
         self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_dexterity_is_dependency_of_urbdial(self):
+        """
+        Dexterity should be installed when we install urbdial
+        """
         self.assertTrue(not self.installer.isProductInstalled('plone.app.dexterity'))
         applyProfile(self.portal, 'imio.urbdial.notarydivision:testing')
         self.assertTrue(self.installer.isProductInstalled('plone.app.dexterity'))
 
 
 class TestInstall(IntegrationTestCase):
-    """Test installation of imio.urbdial.notarydivision into Plone."""
+    """
+    Test installation of imio.urbdial.notarydivision into Plone.
+    """
 
     def setUp(self):
-        """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
         self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
-        """Test if imio.urbdial.notarydivision is installed with portal_quickinstaller."""
+        """
+        Test if imio.urbdial.notarydivision is installed with portal_quickinstaller.
+        """
         self.assertTrue(self.installer.isProductInstalled('imio.urbdial.notarydivision'))
 
     def test_uninstall(self):
-        """Test if imio.urbdial.notarydivision is cleanly uninstalled."""
+        """
+        Test if imio.urbdial.notarydivision is cleanly uninstalled.
+        """
         self.installer.uninstallProducts(['imio.urbdial.notarydivision'])
         self.assertTrue(not self.installer.isProductInstalled('imio.urbdial.notarydivision'))
 
     def test_browserlayer(self):
-        """Test that IImioUrbdialNotarydivisionLayer is registered."""
+        """
+        Test that IImioUrbdialNotarydivisionLayer is registered.
+        """
         from imio.urbdial.notarydivision.interfaces import IImioUrbdialNotarydivisionLayer
         from plone.browserlayer import utils
         self.assertIn(IImioUrbdialNotarydivisionLayer, utils.registered_layers())
 
 
 class TestSetup(unittest.TestCase):
-    """ Test custom code of setuphandlers.py. """
+    """
+    Test custom code of setuphandlers.py.
+    """
 
     layer = NAKED_PLONE_INTEGRATION
 
@@ -63,8 +74,29 @@ class TestSetup(unittest.TestCase):
         applyProfile(self.portal, 'imio.urbdial.notarydivision:testing')
 
     def test_plone_root_default_objects_deleted(self):
+        """
+        We have to get rid of plone root default objects.
+        """
         root_object_ids = self.portal.objectIds()
         self.assertTrue('news' not in root_object_ids)
         self.assertTrue('events' not in root_object_ids)
         self.assertTrue('front-page' not in root_object_ids)
         self.assertTrue('Members' not in root_object_ids)
+
+    def test_notarydivisions_folder_created(self):
+        """
+        """
+        self.assertTrue('notarydivisions' in self.portal.objectIds())
+        divnot_folder = self.portal.notarydivisions
+        self.assertTrue(divnot_folder.portal_type == 'Folder')
+
+    def test_notarydivisions_folder_allowed_types(self):
+        """
+        The notarydivisions folder should only contains NotaryDivision objects.
+        """
+        portal_types = api.portal.get_tool('portal_types')
+        divnot_type = portal_types.notarydivision
+        divnot_folder = self.portal.notarydivisions
+        allowed_types = divnot_folder.allowedContentTypes()
+        self.assertTrue(len(allowed_types) == 1)
+        self.assertTrue(divnot_type in allowed_types)
