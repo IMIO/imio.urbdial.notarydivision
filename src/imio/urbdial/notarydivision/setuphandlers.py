dbd37bc93ce58ead9214dbd9009fa9d933b91a05
Simon Delcourt <delcourt.simon@gmail.com>
2014-05-06 16:19:59 +0200

    #8008 Create folder 'notarydivisions' which contains all the NotaryDivision objects.

--- src/imio/urbdial/notarydivision/setuphandlers.py
+++ src/imio/urbdial/notarydivision/setuphandlers.py
@@ -2,8 +2,16 @@
 
 from plone import api
 
+from zope.component import queryUtility
+from zope.i18n.interfaces import ITranslationDomain
+
 import logging
 logger = logging.getLogger('imio.urbdial.notarydivision: setuphandlers')
+
+
+def _(msgid, domain, context):
+    translation_domain = queryUtility(ITranslationDomain, domain)
+    return translation_domain.translate(msgid, target_language='fr', default='')
 
 
 def isNotCurrentProfile(context):
@@ -15,12 +23,18 @@ def post_install(context):
     if isNotCurrentProfile(context):
         return
 
-    logger.info("deletePloneRootDefaultObjects : starting...")
+    logger.info('deletePloneRootDefaultObjects : starting...')
     deletePloneRootDefaultObjects(context)
-    logger.info("deletePloneRootDefaultObjects : Done")
+    logger.info('deletePloneRootDefaultObjects : Done')
+    logger.info('createNotarydivisionsFolder : starting...')
+    createNotarydivisionsFolder(context)
+    logger.info('createNotarydivisionsFolder : Done')
 
 
 def deletePloneRootDefaultObjects(context):
+    """
+    Get rid of plone root default objects.
+    """
 
     portal = context.getSite()
 
@@ -28,3 +42,34 @@ def deletePloneRootDefaultObjects(context):
     for _id in object__ids:
         if _id in portal.objectIds():
             api.content.delete(portal[_id])
+
+
+def createNotarydivisionsFolder(context):
+    """
+     Create a folder which will contain all our NotaryDivision objects.
+    """
+
+    portal = context.getSite()
+
+    folder_id = 'notarydivisions'
+    if 'notarydivisions' not in portal.objectIds():
+        portal.invokeFactory(
+            'Folder',
+            id=folder_id,
+            title=_('notarydivisions_folder_title', 'urbdial.divnot', context=portal.REQUEST)
+        )
+        folder = getattr(portal, folder_id)
+        _setFolderAllowedTypes(folder, 'NotaryDivision')
+
+
+def _setFolderAllowedTypes(folder, portal_types):
+    """
+    Set allowed content types of given folder.
+    """
+
+    if type(portal_types) != list:
+        portal_types = [portal_types]
+
+    folder.setConstrainTypesMode(1)
+    folder.setLocallyAllowedTypes(portal_types)
+    folder.setImmediatelyAddableTypes(portal_types)
