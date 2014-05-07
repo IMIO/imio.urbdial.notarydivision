79c92bc3afa28e2960fd83481b8aef38fec9f329
fngaha <franck.o.ngaha@gmail.com>
2014-05-07 15:45:03 +0200

    #8011 map our custom role to our custom permission

---
+++ src/imio/urbdial/notarydivision/tests/test_roles_and_permissions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""permissions tests for this package."""
+
+from imio.urbdial.notarydivision.testing import TEST_INSTALL_INTEGRATION
+from plone import api
+import unittest
+
+
+class TestRolesAndPermissions(unittest.TestCase):
+
+    layer = TEST_INSTALL_INTEGRATION
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_AddNotaryDivision_permission_registration(self):
+        """
+        """
+        registered_permissions = self.portal.acl_users.possible_permissions()
+        self.assertTrue('imio.urbdial.notarydivision: Add NotaryDivision' in registered_permissions)
+
+    def test_NotaryDivisionCreator_role_registration(self):
+        """
+        """
+        portal_roles = self.portal.acl_users.portal_role_manager
+        registered_roles = portal_roles.listRoleIds()
+        self.assertTrue('NotaryDivision Creator' in registered_roles)
+
+    def test_NotaryDivision_rolemap(self):
+        """
+        AddNotaryDivision permission should be given to the 'NotaryDivision Creator' role
+        """
+        role_permissions = self.portal.permissionsOfRole('NotaryDivision Creator')
+        permission_names = [p['name'] for p in role_permissions if p['selected']]
+        self.assertTrue('imio.urbdial.notarydivision: Add NotaryDivision' in permission_names)
+        self.assertTrue(len(permission_names) == 1)
