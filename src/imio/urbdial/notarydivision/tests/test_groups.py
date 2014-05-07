51fd51c243a2375e7b8f0010bbf6377b84804a4f
fngaha <franck.o.ngaha@gmail.com>
2014-05-07 11:19:10 +0200

    #8009 Add test

---
+++ src/imio/urbdial/notarydivision/tests/test_groups.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""groups tests for this package."""
+
+from imio.urbdial.notarydivision.testing import TEST_INSTALL_INTEGRATION
+from plone.app.testing import applyProfile
+from plone import api
+import unittest
+
+
+class TestGroups(unittest.TestCase):
+
+    layer = TEST_INSTALL_INTEGRATION
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_notaries_group_creation(self):
+        self.assertTrue(api.group.get('notaries'))
