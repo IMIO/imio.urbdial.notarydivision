3eab4c0b88aeddbabcd7fe30e8ede4f6b1303f6c
Simon Delcourt <delcourt.simon@gmail.com>
2014-05-14 17:06:24 +0200

    Move remaining ergonomy tests from robot to Browser tests.

--- src/imio/urbdial/notarydivision/tests/test_ergonomy.py
+++ src/imio/urbdial/notarydivision/tests/test_ergonomy.py
@@ -3,6 +3,7 @@
 from imio.urbdial.notarydivision.testing import EXAMPLE_DIVISION_INTEGRATION
 
 from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
 from plone.app.testing import login
 from plone.testing.z2 import Browser
 
@@ -19,13 +20,13 @@ class TestErgonomy(unittest.TestCase):
 
         login(self.portal, TEST_USER_NAME)
         self.browser = Browser(self.portal)
-        self.browserLogin(TEST_USER_NAME)
+        self.browserLogin(TEST_USER_NAME, TEST_USER_PASSWORD)
         self.browser.handleErrors = False
 
-    def browserLogin(self, user):
+    def browserLogin(self, user, password):
         self.browser.open(self.portal.absolute_url() + "/login_form")
         self.browser.getControl(name='__ac_name').value = user
-        self.browser.getControl(name='__ac_password').value = user
+        self.browser.getControl(name='__ac_password').value = password
         self.browser.getControl(name='submit').click()
 
     def test_plone_searchbox_disabled(self):
@@ -52,3 +53,19 @@ class TestErgonomy(unittest.TestCase):
         contents = self.browser.contents
         msg = 'Plone site actions are not hidden'
         self.assertTrue('<li id="siteaction' not in contents, msg)
+
+    def test_plone_portlets_are_hidden(self):
+        self.browser.open(self.portal.absolute_url() + '/@@manage-portlets')
+        contents = self.browser.contents
+        msg = 'Plone site actions are not hidden'
+        self.assertTrue('Manage portlets' in contents, msg)
+        self.assertTrue('<span class="managedPortletActions">' not in contents, msg)
+
+    def test_site_view_redirects_to_notarydivisions_folder(self):
+        self.browser.open(self.portal.absolute_url())
+        self.assertTrue(self.browser.url == 'http://nohost/plone/notarydivisions')
+
+    def test_anonymous_is_redirected_to_login_page(self):
+        self.browser.open(self.portal.absolute_url() + "/logout")
+        self.browser.open(self.portal.absolute_url())
+        self.assertTrue(self.browser.url.endswith('/login'))
