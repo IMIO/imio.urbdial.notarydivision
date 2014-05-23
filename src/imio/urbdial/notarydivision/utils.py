dc350daf360542c3267f9edb6bc5c6cbf1f4a20f
Simon Delcourt <delcourt.simon@gmail.com>
2014-05-23 11:39:45 +0200

    When no translation c$can be found for a label, return it rather than ''.

--- src/imio/urbdial/notarydivision/utils.py
+++ src/imio/urbdial/notarydivision/utils.py
@@ -6,4 +6,4 @@ from zope.i18n.interfaces import ITranslationDomain
 
 def translate(msgid, domain='urbdial.divnot'):
     translation_domain = getUtility(ITranslationDomain, domain)
-    return translation_domain.translate(msgid, target_language='fr', default='')
+    return translation_domain.translate(msgid, target_language='fr', default=msgid)
