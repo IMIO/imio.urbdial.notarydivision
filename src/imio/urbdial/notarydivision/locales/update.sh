a17d43f79e4845cedabc0ef8b49343605d3c041b
Simon Delcourt <delcourt.simon@gmail.com>
2014-05-23 11:37:15 +0200

    Merge custom translations when updating urbdial.dinot.pot.

--- src/imio/urbdial/notarydivision/locales/update.sh
+++ src/imio/urbdial/notarydivision/locales/update.sh
@@ -1,3 +1,4 @@
 domain=urbdial.divnot
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+i18ndude merge --pot $domain.pot --merge custom.pot
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
