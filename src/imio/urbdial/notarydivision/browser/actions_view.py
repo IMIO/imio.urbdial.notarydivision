0e62276e5c9c2fd0589d40c71ffc4032fcc1f198
Simon Delcourt <delcourt.simon@gmail.com>
2014-07-01 15:03:07 +0200

    Adapt add conntent actions to display them as an 'answer' button under the comment.

--- src/imio/urbdial/notarydivision/browser/actions_view.py
+++ src/imio/urbdial/notarydivision/browser/actions_view.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 from imio.actionspanel.browser.views import ActionsPanelView
 
 
@@ -15,3 +17,24 @@ class UrbdialActionsPanelView(ActionsPanelView):
             'renderEdit',
             'renderOwnDelete',
         )
+
+
+class UrbdialAddContentPanelView(ActionsPanelView):
+    """
+      This manage the view displaying actions on context.
+    """
+    def __init__(self, context, request):
+        super(UrbdialAddContentPanelView, self).__init__(context, request)
+
+        self.SECTIONS_TO_RENDER = (
+            'renderAddContent',
+        )
+
+    def __call__(self):
+        return self.renderAddContent()
+
+    def renderAddContent(self):
+        """
+          Render allowed_content_types coming from portal_type.
+        """
+        return ViewPageTemplateFile("templates/add_actions.pt")(self)
--- src/imio/urbdial/notarydivision/browser/interfaces.py
+++ src/imio/urbdial/notarydivision/browser/interfaces.py
@@ -3,6 +3,7 @@
 from z3c.form.interfaces import ICheckBoxWidget
 
 from zope import schema
+from zope.interface import Interface
 
 
 class ISingleCheckBoxForDataGridWidget(ICheckBoxWidget):
@@ -11,3 +12,13 @@ class ISingleCheckBoxForDataGridWidget(ICheckBoxWidget):
 
 class IDataGridBool(schema.interfaces.IBool):
     """Marker interface for DataGridBool field."""
+
+
+class IAvailableDocumentsForGeneration(Interface):
+    """Adapt a context and a request to provide a list of PODTemplate."""
+
+    def get_available_templates(self):
+        """
+        Return a list of PODTemplate UIDs which can be generated on the
+        adapted context.
+        """
