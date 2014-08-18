9c1d1f24c7e1ebb72931bdc41e63f35cc275ca70
Simon Delcourt <delcourt.simon@gmail.com>
2014-08-18 10:07:41 +0200

    Comply more to zope.i18n translate.

--- src/imio/urbdial/notarydivision/utils.py
+++ src/imio/urbdial/notarydivision/utils.py
@@ -6,9 +6,13 @@ from zope.component import getUtility
 from zope.i18n.interfaces import ITranslationDomain
 
 
-def translate(msgid, domain='urbdial.divnot'):
+def translate(msgid, domain='urbdial.divnot', target_language='fr'):
     translation_domain = getUtility(ITranslationDomain, domain)
-    translation = translation_domain.translate(msgid, target_language='fr', default=msgid)
+    translation = translation_domain.translate(
+        msgid,
+        target_language=target_language,
+        default=msgid
+    )
     return translation
 
 
