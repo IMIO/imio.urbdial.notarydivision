e6d04a2f7adbccd545af177a9bcb636d662583f5
Simon Delcourt <delcourt.simon@gmail.com>
2014-08-20 13:02:53 +0200

    Translate workflow transitions.

--- src/imio/urbdial/notarydivision/locales/update.sh
+++ src/imio/urbdial/notarydivision/locales/update.sh
@@ -2,3 +2,5 @@ domain=urbdial.divnot
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude merge --pot $domain.pot --merge custom.pot
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+domain=plone
+i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
